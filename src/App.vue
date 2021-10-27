a4a2f63e047cdf4d95dfa29a8ec3f784e1248456
zytqyb <1713684374@qq.com>
2021-10-27 16:33:30 +0800

    feat: 完成项目初始化包括各种插件集成和axios封装
    初始化项目

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,15 @@
 <template>
-  <h2>{{ $store.state.name }}</h2>
-
-  <router-link to="login">登录</router-link>
-  <router-link to="/main">首页</router-link>
-  <router-view></router-view>
-
-  <el-button>Default</el-button>
-  <el-button type="primary">Primary</el-button>
-  <el-button type="success">Success</el-button>
-  <el-button type="info">Info</el-button>
-  <el-button type="warning">Warning</el-button>
-  <el-button type="danger">Danger</el-button>
-
-  <el-input />
+  <div class="app">
+    <router-view></router-view>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'App',
-  props: {
-    name: {
-      type: String
-    }
-  },
-  setup(props) {
-    //
-  }
+  name: 'App'
 })
 </script>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="less"></style>
